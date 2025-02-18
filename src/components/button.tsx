460ff132bb4dabdc5821bedd8ec354c03d365632
Fernando Gabriel <fernando.gabriel322@hotmail.com>
2025-02-18 16:23:12 -0300

    feat: home screen and project setup

---
+++ src/components/button.tsx
@@ -0,0 +1,18 @@
+import type { ComponentProps, ReactNode } from 'react'
+
+interface ButtonProps extends ComponentProps<'button'> {
+  children: ReactNode
+}
+
+const Button = (props: ButtonProps) => {
+  return (
+    <button
+      className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      {...props}
+    >
+      {props.children}
+    </button>
+  )
+}
+
+export default Button
---
+++ src/components/icon-button.tsx
@@ -0,0 +1,18 @@
+import type { ComponentProps, ReactNode } from 'react'
+
+interface IconButtonProps extends ComponentProps<'button'> {
+  children: ReactNode
+}
+
+const IconButton = (props: IconButtonProps) => {
+  return (
+    <button
+      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      {...props}
+    >
+      {props.children}
+    </button>
+  )
+}
+
+export default IconButton
